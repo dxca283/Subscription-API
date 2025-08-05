a1169dba79d1d126124644aa4ac1f1bea644a481
Peace Maker <vdhmichael2001@gmail.com>
2025-08-05 14:25:06 +0700

    create authorize

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from '../middlewares/auth.middleware.js';
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send('get all users'));
+userRouter.get('/', getUsers);
 
-userRouter.get('/:id', (req, res) => res.send('get user details'));
+userRouter.get('/:id', authorize, getUser);
 
 userRouter.post('/', (req, res) => res.send('create user'));
 
